d619cc22eb5f89681d76cbfc9dade08c3a592dc0
Ramiro Fernandez Savoy <rawii.k@gmail.com>
2025-03-09 03:07:01 -0300

    fix: Disabled complex version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
           <div className="border-white rounded-lg border-2 px-8 py-4">
             <h2 className="text-center text-3xl">Armar equipos</h2>
             <div className="flex flex-col md:flex-row py-4 gap-4 mt-2">
-              <Link href="/teams" className="flex flex-col items-center justify-center bg-red-200 hover:bg-red-400 duration-200 px-4 py-2 w-56 text-black rounded-lg font-medium">              
+              {/* <Link href="/teams" className="flex flex-col items-center justify-center bg-red-200 hover:bg-red-400 duration-200 px-4 py-2 w-56 text-black rounded-lg font-medium">              
                 Version: Compleja
-              </Link>  
+              </Link>   */}
               <Link href="/teams2" className="flex flex-col items-center justify-center bg-blue-200 hover:bg-blue-400 duration-200 px-4 py-2 w-56 text-black rounded-lg font-medium">              
                 Version: Simple
               </Link>        
